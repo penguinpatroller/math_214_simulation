a82efffa88492bec141fac16ddb9c97c7c12afc0
penguinpatroller <camdav@umich.edu>
2019-12-04 14:46:14 -0500

    Added transition matrix to file

--- app.js
+++ app.js
@@ -1,25 +1,25 @@
 'use strict';
 
-var eigen_values = [1, 2, 3, 4, 5, 6];
+const fs = require('fs');
 
-class eig{
-  constructor(val, vec){
-    this.val = val;
-    this.vec = vec;
+const { create, all } = require('mathjs')
+const math = create(all);
+const numeric = require('numeric');
+math.import(numeric, { wrap: true, silent: true });
+const numbers = require('numbers');
+math.import(numbers, { wrap: true, silent: true });
+
+var initial_state_vector = [];
+
+const transition_matrix = math.matrix([.6509,0,0,0,0,0,0], [0,.225,0,0,0,0,0], [0,0,.29922,0,0,0,0],
+  [.3491,.775,.30078,.23,0,0,0], [0,0,0,.6377,1,0,0], [0,0,0,.1323,0,0,1,0], [0,0,.4,0,0,0,1]
+]);
+
+function calculate_all_populations(weeks)
+{
+  if(weeks = 0)
+  {
+    return;
   }
+  
 }
-
-var eig1 = new eig(3, [1,2,3,4,5]);
-var eig2 = new eig(0.5, [1,2,3,4,5]);
-var eig3 = new eig(3, [1,2,3,4,5]);
-var eig4 = new eig(3, [1,2,3,4,5]);
-var eig5 = new eig(3, [1,2,3,4,5]);
-var eig6 = new eig(3, [1,2,3,4,5]);
-
-
-
-var eig_objs = [eig1, eig2, eig3, eig4, eig5, eig6];
-
-eig_objs.forEach( (item) =>
-console.log(item.val)
-);
